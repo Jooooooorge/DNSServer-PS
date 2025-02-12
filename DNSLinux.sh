de6d575b0ecba281996e9d3f97fc1e8994734cf3
Jooooooorge <jorgeaguilaruni@gmail.com>
2025-02-12 15:36:35 -0700

    Update DNSLinux.sh

--- DNSLinux.sh
+++ DNSLinux.sh
@@ -11,37 +11,35 @@ sudo mkdir /etc/bind/zones
 # Nos dirigimos a la carpeta donde se encuentra lo que descargamos
 cd /etc/bind/
 
-# Sobreescribir el archivo name.conf.options
-sudo nano /etc/bind/named.conf.options
-# El contenido debe ser:
-# acl LAN {
-#     192.168.0.0/24;
-# };
-#
-# options {
-#     directory "/var/cache/bind";
-#     allow-query { localhost; LAN; };
-#     forwarders {
-#         8.8.8.8;  # Usamos el DNS de Google como Forwarder
-#     };
-#     recursion yes;  # Permite consultas recursivas
-#     listen-on-v6 { any; };
-# };
+# Sobreescribir el archivo named.conf.options
+sudo bash -c 'cat <<EOF > /etc/bind/named.conf.options
+acl LAN {
+    192.168.0.0/24;
+};
 
+options {
+    directory "/var/cache/bind";
+    allow-query { localhost; LAN; };
+    forwarders {
+        8.8.8.8;  # Usamos el DNS de Google como Forwarder
+    };
+    recursion yes;  # Permite consultas recursivas
+    listen-on-v6 { any; };
+};
+EOF'
 
 # Sobreescribir el archivo named.conf.local
-sudo nano /etc/bind/named.conf.local
-# El contenido debe ser:
-# zone "misitio.com" IN {
-#     type master;
-#     file "/etc/bind/zones/misitio.com";
-# };
-#
-# zone "0.162.198.in-addr.arpa" IN {
-#     type master;
-#     file "/etc/bind/zones/misitio.com.rev";
-# };
+sudo bash -c 'cat <<EOF > /etc/bind/named.conf.local
+zone "misitio.com" IN {
+    type master;
+    file "/etc/bind/zones/misitio.com";
+};
 
+zone "0.162.198.in-addr.arpa" IN {
+    type master;
+    file "/etc/bind/zones/misitio.com.rev";
+};
+EOF'
 
 # Nos dirigimos a la carpeta de zonas
 cd /etc/bind/zones
@@ -49,34 +47,35 @@ cd /etc/bind/zones
 # Copíamos el archivo db.local para crear la zona
 sudo cp /etc/bind/db.local /etc/bind/zones/misitio.com
 
-# Editar el archivo de la zona para el dominio
-sudo nano /etc/bind/zones/misitio.com
-# El contenido debe ser:
-# $TTL    604800
-# @       IN      SOA     misitio.com. root.misitio.com. (
-#                             3         ; Serial
-#                         604800         ; Refresh
-#                         86400         ; Retry
-#                         2419200        ; Expire
-#                         604800 )       ; Negative Cache TTL
-#
-# @       IN      NS      ns.misitio.com.
-# @       IN      A       192.168.0.199
-# ns      IN      A       192.168.0.199
+# Crear el archivo de la zona para el dominio
+sudo bash -c 'cat <<EOF > /etc/bind/zones/misitio.com
+\$TTL    604800
+@       IN      SOA     misitio.com. root.misitio.com. (
+                        3         ; Serial
+                    604800         ; Refresh
+                    86400         ; Retry
+                    2419200        ; Expire
+                    604800 )       ; Negative Cache TTL
+
+@       IN      NS      ns.misitio.com.
+@       IN      A       192.168.0.199
+ns      IN      A       192.168.0.199
+EOF'
 
 # Crear el archivo de la zona inversa
-sudo nano /etc/bind/zones/misitio.com.rev
-# El contenido debe ser:
-# $TTL    604800
-# @       IN      SOA     misitio.com. root.misitio.com. (
-#                             3         ; Serial
-#                         604800         ; Refresh
-#                         86400         ; Retry
-#                         2419200        ; Expire
-#                         604800 )       ; Negative Cache TTL
-#
-# @       IN      NS      ns.misitio.com.
-# 199     IN      PTR     misitio.com.
+sudo bash -c 'cat <<EOF > /etc/bind/zones/misitio.com.rev
+\$TTL    604800
+@       IN      SOA     misitio.com. root.misitio.com. (
+                        3         ; Serial
+                    604800         ; Refresh
+                    86400         ; Retry
+                    2419200        ; Expire
+                    604800 )       ; Negative Cache TTL
+
+@       IN      NS      ns.misitio.com.
+199     IN      PTR     misitio.com.
+EOF'
+
 
 # Reiniciar el servicio para reflejar la edición anterior
 sudo systemctl enable bind9
