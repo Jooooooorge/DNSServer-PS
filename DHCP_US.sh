fab668f34728a52db69b888eb2d5cb335a3a6aef
Jooooooorge <jorgeaguilaruni@gmail.com>
2025-02-17 12:39:13 -0700

    Update DHCP_US.sh

--- DHCP_US.sh
+++ DHCP_US.sh
@@ -10,12 +10,16 @@ sudo apt install isc-dhcp-server -y
 # Configuración de red estatica 
 echo "network:
   version: 2
-  renderer: networkd
   ethernets:
-    enp0s3:
+    eth0:
       dhcp4: no
-      addresses: [192.168.0.10/24]  // **AQUI
-      " | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
+      addresses:
+        - 192.168.0.10/24
+      gateway4: 192.168.0.1
+      nameservers:
+        addresses:
+          - 8.8.8.8
+          - 8.8.4.4" | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
 
 # Aplicar cambios de red
 sudo netplan apply  
@@ -23,8 +27,7 @@ sudo netplan apply
 # Realizar los siguientes cambios en los arhivos de configuración
 # del server DHCP /etc/dhcp/dhcp.conf
 
-echo "
-    default-lease-time 43200;
+echo "default-lease-time 43200;
     max-lease-time 86400;
     option subnet-mask 255.255.255.0;
     option broadcast-address 192.168.0.255; // **AQUI
@@ -34,7 +37,10 @@ echo "
         range 192.168.0.20 192.168.0.30; // **AQUI
         option routers 192.168.0.1; // **AQUI
         option domain-name-servers 8.8.8.8; 
-    } 
-" | sudo tee /etc/dhcp/dhcp.conf > /dev/null
+    } " | sudo tee /etc/dhcp/dhcp.conf > /dev/null
+sudo nano /etc/default/isc-dhcp-server
+INTERFACESv4 = "eth0"
 
 sudo systemctl start isc-dhcp-server
+
+sudo systemctl restart isc-dhpc-server
